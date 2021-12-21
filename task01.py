08ecbbdd6ab572e936799c929efd019b9ff9bb68
“Maksim <maksim_bondarenko2@epam.com>
2021-12-21 11:47:14 +0300

    renamed variables

--- task01.py
+++ task01.py
@@ -1,2 +1,8 @@
-a, b = int(input()), int(input())
-print(a+b, a*b, a**b)
+# Напишите программу, которая считает сумму, произведение и степень двух чисел.
+# Ввод: два целых числа, каждое на отдельной строке.
+# Вывод: сумма, произведение и степень, в одну строку через пробел.
+
+# the beginning of the program
+num1, num2 = int(input()), int(input())
+print(num1 + num2, num1 * num2, num1 ** num2)
+# the end of the program
