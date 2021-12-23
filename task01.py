d5c7072cc7baae2417a241ffc08c0d34a4b112e8
“Maksim <maksim_bondarenko2@epam.com>
2021-12-23 10:39:54 +0300

    deleted #the beginning...

--- task01.py
+++ task01.py
@@ -2,6 +2,5 @@
 # Ввод: два целых числа, каждое на отдельной строке.
 # Вывод: сумма, произведение и степень, в одну строку через пробел.
 
-# the beginning of the program
 num1, num2 = int(input()), int(input())
 print(num1+num2, num1*num2, num1**num2)
